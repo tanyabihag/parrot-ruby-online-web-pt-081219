a654471cd2f4bd11888bfc145d2c9875404a4c2e
Tanya Bihag <tbihag@gmail.com>
2019-08-12 22:36:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot ("Squawk")
+  puts " I love why my parrot #{parrot}"
+  return " I love why my parrot #{parrot}"
+end
+
+parrot
